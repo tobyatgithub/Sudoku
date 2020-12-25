d386e3e31dd11aec5f6d4e1ca87587a369cf1bfa
tobyatgithub <toby.fangyuan@gmail.com>
2020-12-24 22:24:37 -0800

    adding solver function.

--- explore.py
+++ explore.py
@@ -25,10 +25,14 @@ def print_board(board: List[list]) -> None:
                 print("|", end=" ")
         print("")
 
-def find_empty_spot(board: List[list]) -> Tuple[int, int]:
+def find_empty_spot(board: List[list], empty_value=0) -> Tuple[int, int]:
+    """
+    Shall return None if the board is full. 
+    Set empty_value for easy test (and allow other value such as * or _)
+    """
     for i in range(len(board)):
         for j in range(len(board[0])):
-            if board[i][j] == 0:
+            if board[i][j] == empty_value:
                 return (i, j)
 
 def check_is_valid(board: List[list], num: int, position: Tuple[int, int]) -> bool:
@@ -50,9 +54,33 @@ def check_is_valid(board: List[list], num: int, position: Tuple[int, int]) -> bo
                 return False
     return True
 
+def solve(board: List[list]):
+    # base case:
+    to_visit = find_empty_spot(board)
+    if to_visit:
+        row, col = to_visit
+    else: # no remaining empty spot, solved.
+        return True
+
+    for candidate_value in range(1,10):
+        if check_is_valid(board, candidate_value, (row, col)):
+            board[row][col] = candidate_value
+
+            if solve(board):
+                return True
+            
+            board[row][col] = 0
+    return False
+
+# Main
 print_board(board)
 
 # hand test:
 assert check_is_valid(board, 5, (1,1)) == False
 assert check_is_valid(board, 1, (1,1)) == True
 assert check_is_valid(board, 1, (8,8)) == False
+assert find_empty_spot(board, -1) == None
+
+print()
+solve(board)
+print_board(board)
